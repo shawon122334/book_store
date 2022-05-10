0043ae48a3bdd2edcc01c1ad74efc60517a61228
sabbir <shawon.mu11@gmail.com>
2022-05-11 00:55:41 +0600

    bug fixed

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express() 
 const mongoose = require('mongoose')
 const bookRouter = require('./routes/book-routes')
+const cors = require('cors')
 
 // app.use('/',(req,res,next)=>{
 //     res.send('application initialized')
@@ -12,13 +13,13 @@ mongoose.connect('mongodb://localhost:27017/book_store_db')
     .catch(err=> console.error(err))
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/books',bookRouter)
 
 app.use((req,res)=>{
     return res.status(404).send('Not Found')
 })
 
-app.listen(3000,()=>{
-    console.log('Listening on port 3000');
+app.listen(5000,()=>{
+    console.log('Listening on port 5000');
 })
